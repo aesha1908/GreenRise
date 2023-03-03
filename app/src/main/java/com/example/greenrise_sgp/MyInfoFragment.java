48a6aebbd150d6f0a6621f80a714f472ea8a80c5
Hetvi Nanavati <hetvi.nanavati@gmail.com>
2023-03-03 23:09:31 +0530

    3/03/23 3

--- app/src/main/java/com/example/greenrise_sgp/MyInfoFragment.java
+++ app/src/main/java/com/example/greenrise_sgp/MyInfoFragment.java
@@ -2,6 +2,7 @@ package com.example.greenrise_sgp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,8 +10,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MyInfoFragment extends Fragment {
 
@@ -31,17 +36,29 @@ public class MyInfoFragment extends Fragment {
                              Bundle savedInstanceState) {
         FirebaseDatabase database;
         DatabaseReference reference;
-
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_my_info, container, false);
         name = view.findViewById(R.id.Username);
         email = view.findViewById(R.id.Email);
         phone = view.findViewById(R.id.Contact);
         database = FirebaseDatabase.getInstance();
-        reference = database.getReference("Users");
-        name.setText(CurrentUser.currentUser.getName());
-        email.setText(CurrentUser.currentUser.getEmail());
-        phone.setText(CurrentUser.currentUser.getPhone());
+        reference = database.getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        name.setText(snapshot.child("name").getValue().toString());
+                        email.setText(snapshot.child("email").getValue().toString());
+                        phone.setText(snapshot.child("phone").getValue().toString());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
         return view;
     }
 }
